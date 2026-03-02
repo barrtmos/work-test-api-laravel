6d5b257fda21dcec1d095b644ddc02ab1899e692
barrtmos <barrtmos@gmail.com>
2026-03-02 13:36:31 +0200

    fix controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,23 +6,21 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    // Обработка GET с параметрами в URL
     public function testGet(Request $request)
     {
         return response()->json([
             'success' => true,
             'method' => 'GET',
-            'received_query' => $request->query() // name и age упадут сюда автоматически
+            'received_query' => $request->query()
         ]);
     }
 
-    // Обработка POST с JSON-телом
     public function testPost(Request $request)
     {
         return response()->json([
             'success' => true,
             'method' => 'POST',
-            'received_data' => $request->all() // email и event извлекутся из JSON
+            'received_data' => $request->all()
         ]);
     }
 }
